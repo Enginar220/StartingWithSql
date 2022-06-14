3becb9c64c7fade4bc0ea1a9d05800931c810d40
Engincan Göçer <98960286+Enginar220@users.noreply.github.com>
2022-06-14 15:22:48 +0300

    Update basic_quaries.sql

--- basic_quaries.sql
+++ basic_quaries.sql
@@ -1,3 +1,5 @@
+\c test -- Connecting to the database named test where car table stays in.
+
 --We see that there are 57 different car make eventhough there are 1000 rows.And this means at least one car make has more than one product.
 SELECT count(make) FROM  car ;
 SELECT count(distinct make) FROM car ;
@@ -8,7 +10,11 @@ SELECT make, count(id) FROM car GROUP BY 1 ;
 --Here is the top three car make that has the most product amount.
 SELECT make, count(id) FROM car GROUP BY 1 ORDER BY  2 desc LIMIT 3; 
 
---Here is the most expensive...
-SELECT 
+--Here are the makes and their most expensive model's prices.
+ SELECT make, max(price) FROM car GROUP BY make ;
+ 
+ -- Here is the most expensive model and its car make.
+SELECT make, model, price FROM car WHERE price =(
+	SELECT max(price) FROM car );
 
 
